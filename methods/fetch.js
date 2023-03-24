bf433decbcff75629327fa4046877ae3006331e0
nilarnab <nilarnabdebnath@gmail.com>
2023-03-24 20:14:24 +0530

    app kill switch added

--- methods/fetch.js
+++ methods/fetch.js
@@ -1,17 +1,28 @@
 /* make a customized fetch request */
+import { BASE_URL } from "../env"
 
-// import fetch
+
 
 export default async function fetch_home(url, payload) {
     try {
-        var response = await fetch(url, payload)
+        // life check
+        var resp_life = await (await fetch(BASE_URL + 'check_life')).json()
 
-        if (response.status == 200) {
-            return response
+        console.log('resp life', resp_life)
+        if (!resp_life.life) {
+            alert(resp_life.message)
         }
         else {
-            return response.status
+            var response = await fetch(url, payload)
+
+            if (response.status == 200) {
+                return response
+            }
+            else {
+                return response.status
+            }
         }
+
 
     }
     catch (err) {
